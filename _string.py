45a8ef1bc4c380e16aa3146c6cae3bd1158768ba
Sina.F <sinaghahreman84@gmail.com>
2023-05-11 18:03:12 +0330

    Add `string`
    A library for accessing different categories of string in python

---
+++ _string.py
@@ -0,0 +1,8 @@
+import string
+
+
+print("Lowercase Letters: ", string.ascii_lowercase)
+print("Uppercase Letters: ", string.ascii_uppercase)
+print("Whole Letters: ", string.ascii_letters)
+print("Digits: ", string.digits)
+print("Punctuations: ", string.punctuation)
