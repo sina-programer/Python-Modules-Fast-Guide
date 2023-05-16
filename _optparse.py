4336207677a5c8eea162ab614b60b969c85a569f
Sina <sina.programerr@gmail.com>
2023-05-16 15:59:13 +0430

    Update `optparse`
    A library for managing given parameters to your python program
    
    add managing boolean flags

--- _optparse.py
+++ _optparse.py
@@ -4,9 +4,18 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-n", "--name", dest="name", type="string", help="select a name (required)")
 parser.add_option("-t", "--times", dest="times", type="int", help="Several times you want to try?")
+
+parser.add_option("-o", "--on", action="store_true", dest="state")
+parser.add_option("-f", "--off", action="store_false", dest="state")
+
 options, args = parser.parse_args()
 
 if not options.name:
     parser.error('name is not given')
+
+if options.state:
+    print('you turned the state on (-o | --on)')
+else:
+    print('you turned the state off (-f | --off)')
 
 print(options)
